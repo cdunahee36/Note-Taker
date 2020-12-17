81a3e1f6eb66d95d7856994b0a628c8de11c95be
cdunahee36 <dunahee10@gmail.com>
2020-12-16 23:43:14 -0600

    All but readme is finalized.

--- Develop/server.js
+++ Develop/server.js
@@ -8,41 +8,36 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('./'));
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//These are the html get routes that bring them to the server
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
+//These are the api routes that connect and use the index.js file to render the notes onto the page
 app.get('/api/notes', (req, res) => {
   res.json(noteJSON);
 });
 
-
+//This post method is pushing the data from the db.json file into the html body
 app.post('/api/notes', (req, res) => {
-
-const lastId = noteJSON.length ? Math.max(...(noteJSON.map(note => note.id))) : 0;
-const id = lastId + 1;
-noteJSON.push( { id, ...req.body} );
-res.json(noteJSON.slice(-1));
+  const lastId = noteJSON.length ? Math.max(...(noteJSON.map(note => note.id))) : 0;
+  const id = lastId + 1;
+  noteJSON.push( { id, ...req.body} );
+  res.json(noteJSON.slice(-1));
 });
 
-
+//This method allows the user to delete an unwanted note
 app.delete('/api/notes/:id', (req, res) => {
-let note = noteJSON.find( ({ id }) => id === JSON.parse(req.params.id));
-
-noteJSON.splice( noteJSON.indexOf(note), 1);
-res.end("Note deleted");
+  let note = noteJSON.find( ({ id }) => id === JSON.parse(req.params.id));
+  noteJSON.splice( noteJSON.indexOf(note), 1);
+  res.end("Note deleted");
 });
 
-
+//Starts the server and allows you to click on it
 app.listen(PORT, () => console.log("Server listening on: http://localhost:" + PORT));
